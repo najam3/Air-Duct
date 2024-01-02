bb92ed8ed58a8aa4399cf7b87316ec61e10edaf1
najam3 <100590374+najam3@users.noreply.github.com>
2024-01-03 01:13:21 +0500

    .

--- src/components/SmNavbar.jsx
+++ src/components/SmNavbar.jsx
@@ -48,8 +48,17 @@ const SmNavbar = () => {
                 </div>
             </div>
             <div className={`${scrollPosition > 200 ? 'fixed shadow-2xl top-0 w-full z-50' : ''} flex items-center flex-col sm:flex-row gap-2 justify-center bg-neutral100 shadow-2xl py-4 pb-8 lg:pb-4 lg:pt-4`}>
-                <button className='bg-secondary px-12 py-4 sm:text-xl text-white font-semibold w-1/2 sm:w-[260px]'>CALL US</button>
-                <button className='bg-primary px-12 py-4 sm:text-xl text-white font-semibold w-1/2 sm:w-[260px]'>SCEDHULE NOW</button>
+                <button className='bg-secondary px-12 py-4 sm:text-xl text-white font-semibold w-1/2 sm:w-[260px]'>
+                    <Link to={'/contact'}>
+                        CALL US
+                    </Link>
+                    
+                    </button>
+                <button className='bg-primary px-12 py-4 sm:text-xl text-white font-semibold w-1/2 sm:w-[260px]'>
+                        <Link to={'/contact'}>
+                            SCEDHULE NOW
+                        </Link>
+                    </button>
                 <div className={`bg-primary w-full h-4 absolute bottom-0 left-0`}></div>
             </div>
         </nav>
